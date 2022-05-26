a07b607c95f263312e91ada16fb2807c6e714ed4
benbraide <benbraide@yahoo.com>
2022-05-26 19:02:58 +0100

    Fixed references

--- src/inlinejs-pack.ts
+++ src/inlinejs-pack.ts
@@ -1,3 +1,145 @@
-require('@benbraide/inlinejs')
-require('@benbraide/inlinejs-core')
-require('@benbraide/inlinejs-extended/lib/common/index')
+import { AutoBootstrap, GetGlobal, AttributeInterpolator, TextContentInterpolator } from '@benbraide/inlinejs';
+
+import {
+    DataDirectiveHandlerCompact,
+    ComponentDirectiveHandlerCompact,
+    LocalsDirectiveHandlerCompact,
+    RefDirectiveHandlerCompact,
+    PostDirectiveHandlerCompact,
+    UninitDirectiveHandlerCompact,
+    StaticDirectiveHandlerCompact,
+    EffectDirectiveHandlerCompact,
+    CloakDirectiveHandlerCompact,
+    BindDirectiveHandlerCompact,
+    ClassDirectiveHandlerCompact,
+    StyleDirectiveHandlerCompact,
+    TextDirectiveHandlerCompact,
+    HtmlDirectiveHandlerCompact,
+    OnDirectiveHandlerCompact,
+    ModelDirectiveHandlerCompact,
+    ShowDirectiveHandlerCompact,
+    IfDirectiveHandlerCompact,
+    ElseDirectiveHandlerCompact,
+    EachDirectiveHandlerCompact,
+    ComponentMagicHandlerCompact,
+    LocalsMagicHandlerCompact,
+    RefsMagicHandlerCompact,
+    ScopeMagicHandlerCompact,
+    ProxyMagicHandlerCompact,
+    NativeMagicHandlerCompact,
+    WaitMagicHandlerCompact,
+    StreamMagicHandlerCompact,
+    StaticMagicHandlerCompact,
+    UnoptimizedMagicHandlerCompact,
+    WatchMagicHandlerCompact,
+    ArithmeticMagicHandlerCompact,
+    RelationalMagicHandlerCompact,
+    LogicalMagicHandlerCompact,
+    NextTickMagicHandlerCompact,
+    PickMagicHandlerCompact,
+    ClassMagicHandlerCompact,
+    EvaluateMagicHandlerCompact,
+    DomMagicHandlerCompact
+} from '@benbraide/inlinejs-core';
+
+import {
+    ResourceConcept,
+    AttrDirectiveHandlerCompact,
+    IntersectionDirectiveHandlerCompact,
+    TickDirectiveHandlerCompact,
+    FormDirectiveHandlerCompact,
+    StateDirectiveHandlerCompact,
+    OverlayDirectiveHandlerCompact,
+    MouseDirectiveHandlerCompact,
+    KeyboardDirectiveHandlerCompact,
+    FormatMagicHandlerCompact,
+    GetMagicHandlerCompact,
+    FetchMagicHandlerCompact,
+    OverlayMagicHandlerCompact
+} from '@benbraide/inlinejs-extended/lib/esm';
+
+//Begin: inlinejs
+
+AutoBootstrap();
+
+GetGlobal().AddAttributeProcessor(AttributeInterpolator);
+GetGlobal().AddTextContentProcessor(TextContentInterpolator);
+
+//End: inlinejs
+
+
+//Begin: inlinejs-core
+
+DataDirectiveHandlerCompact();
+ComponentDirectiveHandlerCompact();
+LocalsDirectiveHandlerCompact();
+RefDirectiveHandlerCompact();
+
+PostDirectiveHandlerCompact();
+UninitDirectiveHandlerCompact();
+
+StaticDirectiveHandlerCompact();
+EffectDirectiveHandlerCompact();
+
+CloakDirectiveHandlerCompact();
+
+BindDirectiveHandlerCompact();
+ClassDirectiveHandlerCompact();
+StyleDirectiveHandlerCompact();
+
+TextDirectiveHandlerCompact();
+HtmlDirectiveHandlerCompact();
+OnDirectiveHandlerCompact();
+ModelDirectiveHandlerCompact();
+
+ShowDirectiveHandlerCompact();
+
+IfDirectiveHandlerCompact();
+ElseDirectiveHandlerCompact();
+EachDirectiveHandlerCompact();
+
+ComponentMagicHandlerCompact();
+LocalsMagicHandlerCompact();
+RefsMagicHandlerCompact();
+ScopeMagicHandlerCompact();
+ProxyMagicHandlerCompact();
+NativeMagicHandlerCompact();
+WaitMagicHandlerCompact();
+StreamMagicHandlerCompact();
+
+StaticMagicHandlerCompact();
+UnoptimizedMagicHandlerCompact();
+WatchMagicHandlerCompact();
+
+ArithmeticMagicHandlerCompact();
+RelationalMagicHandlerCompact();
+LogicalMagicHandlerCompact();
+
+NextTickMagicHandlerCompact();
+PickMagicHandlerCompact();
+ClassMagicHandlerCompact();
+EvaluateMagicHandlerCompact();
+DomMagicHandlerCompact();
+
+//End: inlinejs-core
+
+
+//Begin: inlinejs-extended
+
+GetGlobal().SetConcept('resource', new ResourceConcept());
+    
+AttrDirectiveHandlerCompact();
+IntersectionDirectiveHandlerCompact();
+TickDirectiveHandlerCompact();
+FormDirectiveHandlerCompact();
+StateDirectiveHandlerCompact();
+OverlayDirectiveHandlerCompact();
+MouseDirectiveHandlerCompact();
+KeyboardDirectiveHandlerCompact();
+
+FormatMagicHandlerCompact();
+GetMagicHandlerCompact();
+FetchMagicHandlerCompact();
+OverlayMagicHandlerCompact();
+
+//End: inlinejs-extended
